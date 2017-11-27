d32f90b31f95afd3728576cfeb9081ed37fc135a
Maciej001 <m.nowakowskipl@gmail.com>
2017-11-27 13:23:21 +0000

    State moved to Navigation

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -3,10 +3,21 @@ import "./Navigation.css";
 import Selection from "./Selection"
 
 class Navigation extends React.Component {
+  state = {
+    genre: ''
+  }
+
+  onGenreChange = event => {
+    this.setState({ genre: event.target.value });
+  }
+
   render() {
     return (
       <div className="navigation">
-        <Selection />
+        <Selection
+          genre={this.state.genre}
+          onGenreChange={this.onGenreChange}
+        />
       </div>
     )
   }
--- src/main/navigation/Selection.js
+++ src/main/navigation/Selection.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
-class Selection extends React.Component {
-  state = {
-    value: null
-  }
-
-  onChange = e => {
-    this.setState({ value: e.target.value });
-  }
-
-  render() {
-    return (
-      <select value={this.state.value} onChange={this.onChange}>
-        <option value="comedy">Comedy</option>
-        <option value="action">Action</option>
-        <option value="drama">Drama</option>
-      </select>
-    )
-  }
-}
+const Selection = ({ genre, onGenreChange }) => (
+  <select value={genre} onChange={onGenreChange}>
+    <option value="comedy">Comedy</option>
+    <option value="action">Action</option>
+    <option value="drama">Drama</option>
+  </select>
+)
 
 export default Selection;
